870f4da1d013e72311c5792e9e325efd80bd29e8
Agung Wiyono <wiyonoagung1@gmail.com>
2020-02-10 08:44:32 +0700

    Porting finished, need more test on templating

--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,20 @@ Licence: GPLv3
 """
 
 from flask import Flask
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.pymongo import PyMongo
-from flask.ext.login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
-#Configuration of application, see configuration.py, choose one and uncomment.
-#app.config.from_object('configuration.ProductionConfig')
+# Configuration of application, see configuration.py, choose one and uncomment.
+# app.config.from_object('configuration.ProductionConfig')
 app.config.from_object('app.configuration.DevelopmentConfig')
-#app.config.from_object('configuration.TestingConfig')
+# app.config.from_object('configuration.TestingConfig')
 
-bs = Bootstrap(app) #flask-bootstrap
-db = SQLAlchemy(app) #flask-sqlalchemy
+bs = Bootstrap(app)  # flask-bootstrap
+db = SQLAlchemy(app)  # flask-sqlalchemy
 
 lm = LoginManager()
 lm.setup_app(app)
--- app/forms.py
+++ app/forms.py
@@ -4,15 +4,18 @@ Python Aplication Template
 Licence: GPLv3
 """
 
-from flask.ext.wtf import Form, TextField, TextAreaField, DateTimeField, PasswordField
-from flask.ext.wtf import Required
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, TextField, DateTimeField, PasswordField
+from wtforms.validators import InputRequired
 
-class ExampleForm(Form):
-	title = TextField(u'Título', validators = [Required()])
-	content = TextAreaField(u'Conteúdo')
-	date = DateTimeField(u'Data', format='%d/%m/%Y %H:%M')
-	#recaptcha = RecaptchaField(u'Recaptcha')
 
-class LoginForm(Form):
-	user = TextField(u'Usuário', validators = [Required()])
-	password = PasswordField(u'Senha', validators = [Required()])
+class ExampleForm(FlaskForm):
+    title = TextField(u'Título', validators=[InputRequired()])
+    content = TextAreaField(u'Conteúdo')
+    date = DateTimeField(u'Data', format='%d/%m/%Y %H:%M')
+    # recaptcha = RecaptchaField(u'Recaptcha')
+
+
+class LoginForm(FlaskForm):
+    user = TextField(u'Usuário', validators=[InputRequired()])
+    password = PasswordField(u'Senha', validators=[InputRequired()])
